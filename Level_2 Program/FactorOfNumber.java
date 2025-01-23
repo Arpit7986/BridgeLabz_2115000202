5619889ead6149efeb16590802ed098d2e01007a
Arpit7986 <arpitjain4746@gmail.com>
2025-01-23 21:01:41 +0530

    Assignment Level-2 Added

---
+++ Level_2 Program/FactorOfNumber.java
@@ -0,0 +1,12 @@
+public class FactorOfNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
